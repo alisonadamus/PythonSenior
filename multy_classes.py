4f0bd75a95484b0e43a31c97000f9cf5e6636a58
alisonadamus <alisaadamus.aa@gmail.com>
2025-10-21 18:08:29 +0300

    add new file

--- multy_classes.py
+++ multy_classes.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Player():
     def __init__(self, x, y, radius, color):
@@ -21,22 +22,45 @@ class Player():
         if key[pygame.K_DOWN]:
             self.y += 5
 
+
+class Enemy:
+    def __init__(self):
+        x = random.randint(0, 400)
+        y = random.randint(0, 300)
+        self.rect = pygame.Rect(x, y, 40, 40)
+        self.color = (255, 0, 0)
+        self.speed = random.choice([2, 3, 4])
+        self.direction = random.choice([-1, 1])
+
+    def update(self):
+        self.rect.y += self.speed * self.direction
+        if self.rect.top <= 0 or self.rect.bottom >= 600:
+            self.direction *= -1
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
+
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
-player = Player(400, 300, 50, (35, 255, 150))
+player = Player(400, 300, 20, (35, 255, 150))
+enemies = [Enemy() for _ in range(5)]
 player.draw()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
-
-
     screen.fill((255,255,5))
     player.move()
     player.draw()
+
+    for enemy in enemies:
+        enemy.update()
+        enemy.draw(screen)
+        if enemy.rect.x in range(player.x - 30, player.x + 30) and enemy.rect.y in  range(player.y - 30, player.y + 30):
+            enemy.color = (0, 255, 0)
 
     pygame.display.flip()
     clock.tick(60)
